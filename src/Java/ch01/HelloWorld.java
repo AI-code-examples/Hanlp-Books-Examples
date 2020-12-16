487b4c0114a99072c09e65802b01a48f45266357
zYx.Tom <zhuyuanxiang@gmail.com>
2020-12-16 17:26:14 +0800

    Ch01.Java.End

---
+++ src/Java/ch01/HelloWorld.java
@@ -0,0 +1,13 @@
+package Java.ch01;
+
+
+import com.hankcs.hanlp.HanLP;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print(HanLP.segment("你好，欢迎使用 Python 中调用 HanLP 的 API 接口！"));
+        HanLP.Config.enableDebug();
+        System.out.println(HanLP.segment("王国维和服务员"));
+    }
+}
