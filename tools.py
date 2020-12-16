417106d7f56b3f5ee493903b055dae9202d2b648
zYx.Tom <zhuyuanxiang@gmail.com>
2020-12-16 18:33:43 +0800

    tools.show_subtitle()

---
+++ tools.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-    
+"""
+@Author     :   zYx.Tom
+@Contact    :   526614962@qq.com
+@site       :   https://zhuyuanxiang.github.io
+---------------------------
+@Software   :   IDEA
+@Project    :   Hanlp-Books-Examples
+@File       :   tools.py
+@Version    :   v0.1
+@Time       :   2020-12-16 17:57
+@License    :   (C)Copyright 2020-2020, zYx.Tom
+@Reference  :   
+@Desc       :   
+@理解：
+"""
+
+def show_subtitle(message):
+    # 输出运行模块的子标题
+    print('-' * 15, '>' + message + '<', '-' * 15)
+    pass
