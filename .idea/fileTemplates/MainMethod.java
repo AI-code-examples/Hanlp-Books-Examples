b7c04fb69f475fe405fbfb74079781937d9cb922
zYx.Tom <zhuyuanxiang@gmail.com>
2020-12-20 13:11:47 +0800

    Ch02.IDEA.Config.End

--- .idea/fileTemplates/MainMethod.java
+++
@@ -1,7 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-#parse("File Header.java")
-public class ${NAME} {
-    public static void main(String[] args){
-        // add 
-    }
-}
