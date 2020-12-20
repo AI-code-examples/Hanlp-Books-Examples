12747341e797c635295acb30e58ee8e8d911cb36
zYx.Tom <zhuyuanxiang@gmail.com>
2020-12-20 10:36:17 +0800

    test.get project root path

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,10 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("你好，中国!");
+        System.out.println(test.class.getResource("/").getPath());
+        System.out.println(test.class.getClassLoader().getResource("").getPath());
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("user.home"));
     }
-    
+
 }
