8986a2a2096704453748b8dcabb977feaffe3e7b
zYx.Tom <zhuyuanxiang@gmail.com>
2020-12-20 10:35:48 +0800

    Tools.Comment

--- src/main/java/tools/Tools.java
+++ src/main/java/tools/Tools.java
@@ -3,19 +3,34 @@ package tools;
 import java.util.Collections;
 
 public class Tools {
+    /**
+     * 输出运行模块的子标题
+     * @param message 标题的内容
+     */
     public static void show_subtitle(String message) {
-        // 输出运行模块的子标题
         System.out.println(
                 String.join("", Collections.nCopies(15, "-")) + '>'
                         + message
                         + '<' + String.join("", Collections.nCopies(15, "-")));
     }
 
+    /**
+     * 输出运行模块的标题
+     * @param message 标题的内容
+     */
     public static void show_title(String message) {
-        // 输出运行模块的子标题
         System.out.println(
                 String.join("", Collections.nCopies(15, "=")) + '>'
                         + message
                         + '<' + String.join("", Collections.nCopies(15, "=")));
+    }
+
+
+    /**
+     * 获取项目路径
+     * @return 返回当前项目的绝对路径
+     */
+    public static String get_root_path() {
+        return System.getProperty("user.dir");
     }
 }
