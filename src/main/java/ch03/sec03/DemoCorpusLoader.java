9eba3ad19497968187f0644ee9f476e387ea7374
zYx.Tom <zhuyuanxiang@gmail.com>
2020-12-21 18:18:29 +0800

    Ch03.Sec02.Java.End

---
+++ src/main/java/ch03/sec03/DemoCorpusLoader.java
@@ -0,0 +1,30 @@
+package ch03.sec03;
+
+import com.hankcs.hanlp.corpus.document.CorpusLoader;
+import com.hankcs.hanlp.corpus.document.sentence.word.IWord;
+import com.hankcs.hanlp.corpus.io.IOUtil;
+
+import java.util.List;
+
+/**
+ * 手工加载语料库
+ */
+public class DemoCorpusLoader {
+    public static String MY_CWS_CORPUS_PATH = "data/test/my_cws_corpus.txt";
+
+    static {
+        if (!IOUtil.isFileExisted(MY_CWS_CORPUS_PATH))
+            IOUtil.saveTxt(MY_CWS_CORPUS_PATH, "商品 和 服务\n" + "商品 和服 物美价廉\n" + "服务 和 货币");
+    }
+
+    public static void main(String[] args) {
+        List<List<IWord>> sentenceList = CorpusLoader.convert2SentenceList(MY_CWS_CORPUS_PATH);
+        for (List<IWord> sentence : sentenceList) {
+            System.out.println("sentence: " + sentence);
+            System.out.print("word: ");
+            for (IWord word : sentence)
+                System.out.print(word + ", ");
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/ch03/sec03/DemoNgramSegment.java
@@ -0,0 +1,70 @@
+package ch03.sec03;
+
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.corpus.dictionary.NatureDictionaryMaker;
+import com.hankcs.hanlp.corpus.document.CorpusLoader;
+import com.hankcs.hanlp.corpus.document.sentence.word.IWord;
+import com.hankcs.hanlp.dictionary.CoreBiGramTableDictionary;
+import com.hankcs.hanlp.dictionary.CoreDictionary;
+import com.hankcs.hanlp.seg.Dijkstra.DijkstraSegment;
+import com.hankcs.hanlp.seg.Segment;
+import com.hankcs.hanlp.seg.Viterbi.ViterbiSegment;
+
+import java.util.List;
+
+import static ch03.sec03.DemoCorpusLoader.MY_CWS_CORPUS_PATH;
+
+public class DemoNgramSegment {
+    public static final String MY_MODEL_PATH = "data/model/my_cws_model";
+    public static final String MSR_MODEL_PATH = "data/model/msr_ngram";
+    public static final String MSR_TRAIN_PATH = "data/test/icwb2-data/gold/msr_training_words.utf8";
+
+
+    public static void main(String[] args) {
+        // 第一次执行会报出警告，然后会转换 txt 文件为 bin 文件，后面就不再报错
+        // 连续执行两次时，第二次使用的是第一次载入的 bin 文件
+        trainBigram(MY_CWS_CORPUS_PATH, MY_MODEL_PATH);
+        loadBigram(MY_MODEL_PATH);
+        trainBigram(MSR_TRAIN_PATH, MSR_MODEL_PATH);
+        loadBigram(MSR_MODEL_PATH);
+    }
+
+    /**
+     * 训练 bigram 模型
+     *
+     * @param corpusPath 语料库路径
+     * @param modelPath  模型保存路径
+     */
+    public static void trainBigram(String corpusPath, String modelPath) {
+        List<List<IWord>> sentenceList = CorpusLoader.convert2SentenceList(corpusPath);
+        for (List<IWord> sentence : sentenceList)
+            for (IWord word : sentence)
+                if (word.getLabel() == null) word.setLabel("n");  // 赋予每个单词一个虚拟的名词词性
+        final NatureDictionaryMaker dictionaryMaker = new NatureDictionaryMaker();
+        dictionaryMaker.compute(sentenceList);
+        dictionaryMaker.saveTxtTo(modelPath);
+    }
+
+    public static Segment loadBigram(String modelPath) {
+        return loadBigram(modelPath, true, true);
+    }
+
+    private static Segment loadBigram(String modelPath, boolean verbose, boolean viterbi) {
+//        HanLP.Config.enableDebug();
+        HanLP.Config.CoreDictionaryPath = modelPath + ".txt";
+        HanLP.Config.BiGramDictionaryPath = modelPath + ".ngram.txt";
+        if (verbose) {
+            HanLP.Config.ShowTermNature = false;
+            System.out.println("「商品」的词频：" + CoreDictionary.getTermFrequency("商品"));
+            System.out.println("「商品@和」的频次：" + CoreBiGramTableDictionary.getBiFrequency("商品", "和"));
+            Segment segment = new DijkstraSegment()
+                    .enableAllNamedEntityRecognize(false)   // 禁用命名实体识别
+                    .enableCustomDictionary(false);         // 禁用用户词典
+            System.out.println(segment.seg("商品和服务"));
+            System.out.println(segment.seg("货币和服务"));
+        }
+        Segment viterbiSegment = new ViterbiSegment().enableAllNamedEntityRecognize(false).enableCustomDictionary(false);
+        Segment dijkstraSegment = new DijkstraSegment().enableAllNamedEntityRecognize(false).enableCustomDictionary(false);
+        return viterbi ? viterbiSegment : dijkstraSegment;
+    }
+}
